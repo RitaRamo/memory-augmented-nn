6611b1ecaa75c26903c99adffde2ba82b350410b
RitaRamos <rita.mparada.ramos@gmail.com>
2020-12-23 17:45:36 +0000

    add fastext

--- image_captioning/models.py
+++ image_captioning/models.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 import torchvision
+import fasttext
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -66,7 +67,7 @@ class Attention(nn.Module):
         self.encoder_att = nn.Linear(encoder_dim, attention_dim)  # linear layer to transform encoded image
         self.decoder_att = nn.Linear(decoder_dim, attention_dim)  # linear layer to transform decoder's output
         self.full_att = nn.Linear(attention_dim, 1)  # linear layer to calculate values to be softmax-ed
-        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
         self.softmax = nn.Softmax(dim=1)  # softmax layer to calculate weights
 
     def forward(self, encoder_out, decoder_hidden):
@@ -79,7 +80,7 @@ class Attention(nn.Module):
         """
         att1 = self.encoder_att(encoder_out)  # (batch_size, num_pixels, attention_dim)
         att2 = self.decoder_att(decoder_hidden)  # (batch_size, attention_dim)
-        att = self.full_att(self.relu(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
+        att = self.full_att(self.tanh(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
         alpha = self.softmax(att)  # (batch_size, num_pixels)
         attention_weighted_encoding = (encoder_out * alpha.unsqueeze(2)).sum(dim=1)  # (batch_size, encoder_dim)
 
@@ -91,7 +92,7 @@ class DecoderWithAttention(nn.Module):
     Decoder.
     """
 
-    def __init__(self, attention_dim, embed_dim, decoder_dim, vocab_size, encoder_dim=2048, dropout=0.5):
+    def __init__(self, attention_dim, embed_dim, decoder_dim, vocab_size, token_to_id, encoder_dim=2048, dropout=0.5):
         """
         :param attention_dim: size of attention network
         :param embed_dim: embedding size
@@ -107,6 +108,7 @@ class DecoderWithAttention(nn.Module):
         self.embed_dim = embed_dim
         self.decoder_dim = decoder_dim
         self.vocab_size = vocab_size
+        self.token_to_id = token_to_id
         self.dropout = dropout
 
         self.attention = Attention(encoder_dim, decoder_dim, attention_dim)  # attention network
@@ -125,17 +127,34 @@ class DecoderWithAttention(nn.Module):
         """
         Initializes some parameters with values from the uniform distribution, for easier convergence.
         """
-        self.embedding.weight.data.uniform_(-0.1, 0.1)
         self.fc.bias.data.fill_(0)
         self.fc.weight.data.uniform_(-0.1, 0.1)
 
-    def load_pretrained_embeddings(self, embeddings):
-        """
-        Loads embedding layer with pre-trained embeddings.
+        #init embedding layer
+        fasttext_embeddings = fasttext.load_model('embeddings/fasttext/wiki.en.bin')
+        pretrained_embeddings = _get_fasttext_embeddings_matrix(fasttext_embeddings, embed_dim, token_to_id)
+        self.embedding.weight.data.copy_(
+            torch.from_numpy(pretrained_embeddings))
 
-        :param embeddings: pre-trained embeddings
-        """
-        self.embedding.weight = nn.Parameter(embeddings)
+        # pretrained embedings are not trainable by default
+        self.embedding.weight.requires_grad = False
+
+    
+    def _get_fasttext_embeddings_matrix(embeddings):
+    # reduce the matrix of pretrained:embeddings according to dataset vocab
+        print("loading fasttext embeddings")
+
+        embeddings_matrix = np.zeros(
+            (self.vocab_size, self.embed_dim))
+
+        for word, id in self.token_to_id.items():
+            try:
+                embeddings_matrix[id] = embeddings.get_word_vector(word)
+            except:
+                print("How? Fastext has embedding for each token, hence this exception should not happen")
+                pass
+
+        return embeddings_matrix
 
     def fine_tune_embeddings(self, fine_tune=True):
         """
