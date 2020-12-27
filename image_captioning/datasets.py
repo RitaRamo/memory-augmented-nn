0c1937daec3e5bad648183d8f70050809e876906
RitaRamos <rita.mparada.ramos@gmail.com>
2020-12-27 21:22:19 +0000

    add image retrieval

--- image_captioning/datasets.py
+++ image_captioning/datasets.py
@@ -5,68 +5,63 @@ import json
 import os
 from PIL import Image
 import torchvision.transforms as transforms
-import faiss                   # make faiss available
+import faiss  
+import numpy as np                 # make faiss available
 
-#class Faiss
-#class CaptionDatasetAvg()
-#class CaptionDatasetAvg()
 
-# d = self.encoder.encoder_dim
-# index = faiss.IndexFlatL2(d)
-# images_ids = []
+class TrainRetrievalDataset(Dataset):
+    """
+    A PyTorch Dataset class to be used in a PyTorch DataLoader to create batches.
+    """
 
-# train_dataset = get_dataset(PATH_DATASETS_RSICD + "train_coco_format.json")
-# #train_dataset = get_dataset(PATH_DATASETS_RSICD + "val_coco_format.json")
+    def __init__(self, data_folder, data_name):
+        """
+        :param data_folder: folder where data files are stored
+        :param data_name: base name of processed datasets
+        :param split: split, one of 'TRAIN', 'VAL', or 'TEST'
+        :param transform: image transform pipeline
+        """
+        self.data_folder=data_folder
 
-# transform = transforms.Compose([
-#     transforms.ToTensor(),
-#     transforms.Normalize(mean=[0.485, 0.456, 0.406],  # mean=IMAGENET_IMAGES_MEAN, std=IMAGENET_IMAGES_STD
-#                          std=[0.229, 0.224, 0.225])
-# ])
+        with open(os.path.join(data_folder, "TRAIN" + '_IMGPATHS_' + data_name + '.json'), 'r') as j:
+            self.imgpaths = json.load(j)
 
-# for values in train_dataset["images"]:
+        self.imgpaths=self.imgpaths[:10]
+        print("self images", self.imgpaths)
+        #TODO:REMOVE
 
-#     img_name = values["file_name"]
-#     image_id = values["id"]
+        # Total number of datapoints
+        self.dataset_size = len(self.imgpaths)
+        #print("this is the actual len on begin init", self.dataset_size)
 
-#     image_name = PATH_RSICD + \
-#         "raw_dataset/RSICD_images/" + img_name
-#     image = cv2.imread(image_name)
-#     image = transform(image)
-#     image = image.unsqueeze(0)
 
-#     images_ids.append(image_id)
+        self.transform = transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],  # mean=IMAGENET_IMAGES_MEAN, std=IMAGENET_IMAGES_STD
+                                     std=[0.229, 0.224, 0.225])
+        ])
 
-#     encoder_output = self.encoder(image)
-#     encoder_output = encoder_output.view(1, -1, encoder_output.size()[-1])
-#     mean_encoder_output = encoder_output.mean(dim=1)
-#     index.add(mean_encoder_output.numpy())
+    def __getitem__(self, i):
+        # Remember, the Nth caption corresponds to the (N // captions_per_image)th image    
+        img = Image.open(self.data_folder+"/"+self.imgpaths[i])
+        img = self.transform(img)
+        #print("i of retrieval dataset",i)
+        return img, i
+   
+    def __len__(self):
+        #print("this is the actual len on __len", self.dataset_size)
+        return self.dataset_size
 
-# dict_imageid_refs = defaultdict(list)
-# all_captions = []
-# for ref in train_dataset["annotations"]:
-#     image_id = ref["image_id"]
-#     caption = ref["caption"]
-#     all_captions.append(caption)
-#     dict_imageid_refs[image_id].append(caption)
-
-# counter_refs = Counter(all_captions)
 
 
 class ImageRetrieval():
 
-    def __init__(self, dim_examples, data_folder, data_name, encoder):
-        self.index = faiss.IndexFlatL2(dim_examples) #datastore
-        self.lookup_targets = {}
-        #data
-        with open(os.path.join(data_folder, 'TRAIN' + '_CAPTIONS_' + data_name + '.json'), 'r') as j:
-            target_examples = json.load(j) 
-
-        with open(os.path.join(data_folder, 'TRAIN' + '_IMGPATHS_' + data_name + '.json'), 'r') as j:
-            input_examples = json.load(j) 
-
-        self.data_folder=data_folder
-        self._add_examples(input_examples, target_examples)
+    def __init__(self, dim_examples, encoder, train_dataloader_images):
+        print("self dim exam", dim_examples)
+        self.datastore = faiss.IndexFlatL2(dim_examples) #datastore
+        self.encoder= encoder
 
         self.transform = transforms.Compose([
                     transforms.Resize(256),
@@ -76,39 +71,50 @@ class ImageRetrieval():
                                         std=[0.229, 0.224, 0.225])
         ])
 
-        self.encoder= encoder
+        #data
+        self.imgs_indexes_of_dataloader = torch.tensor([], dtype=torch.int32)
+        #print("len img dataloader", self.imgs_indexes_of_dataloader.size())
+        self._add_examples(train_dataloader_images)
+        #print("len img dataloader final", self.imgs_indexes_of_dataloader.size())
+        print("como ficou img dataloader final", self.imgs_indexes_of_dataloader)
 
-    def _add_examples(self, input_examples, target_examples):
-        input_id = 0
-        for i in input_examples:
-            img_path = input_examples[i]
-            img_captions = target_examples[i+5] #5 captions
 
-            #convert to high dimensional vector
-            img = Image.open(self.data_folder+"/"+img_path) #complete_path of image
-            img = self.transform(img)
-
-            encoder_output = self.encoder(image)
-            encoder_output = encoder_output.view(1, -1, encoder_output.size()[-1])
-            input_image = encoder_output.mean(dim=1)
-
-            self.lookup_targets[input_id] = img_caption
-
+    def _add_examples(self, train_dataloader_images):
+        print("\nadding input examples to datastore (retrieval)")
+        for i, (imgs, imgs_indexes) in enumerate(train_dataloader_images):
             #add to the datastore
-            index.add(input_image.numpy())
-            input_id+=1
+            encoder_output = self.encoder(imgs)
 
-    def retrieve_nearest(self, query_img, k=1):
-        D, I = index.search(query_img, k)     # actual search
-        nearest_target = self.lookup_targets[I[0,0]]
-        return nearest_target
+            encoder_output = encoder_output.view(encoder_output.size()[0], -1, encoder_output.size()[-1])
+            input_img = encoder_output.mean(dim=1)
+            
+            self.datastore.add(input_img.numpy())
+
+            if i%5==0:
+                print("i and img index of ImageRetrival",i, imgs_indexes)
+                print("n of examples", self.datastore.ntotal)
+            self.imgs_indexes_of_dataloader= torch.cat((self.imgs_indexes_of_dataloader,imgs_indexes))
 
 
-        #return I #nearest image
 
-        #self.lookup_targets[I] # target
+    def retrieve_nearest_for_train_query(self, query_img, k=2):
+        print("self query img", query_img)
+        D, I = self.datastore.search(query_img, k)     # actual search
+        print("all nearest", I)
+        print("I firt", I[:,0])
+        print("if you choose the first", self.imgs_indexes_of_dataloader[I[:,0]])
+        nearest_input = self.imgs_indexes_of_dataloader[I[:,1]]
+        print("the nearest input is actual the second for training", nearest_input)
+        #nearest_input = I[0,1]
+        #print("actual nearest_input", nearest_input)
+        return nearest_input
 
-
+    def retrieve_nearest_for_val_or_test_query(self, query_img, k=1):
+        D, I = self.datastore.search(query_img, k)     # actual search
+        nearest_input = self.imgs_indexes_of_dataloader[I[:,0]]
+        print("all nearest", I)
+        print("the nearest input", nearest_input)
+        return I
 
 
 class NearestCaptionAvgDataset(Dataset):
@@ -123,6 +129,8 @@ class NearestCaptionAvgDataset(Dataset):
         :param split: split, one of 'TRAIN', 'VAL', or 'TEST'
         :param transform: image transform pipeline
         """
+        print("entrei no infico aqui")
+
         self.data_folder=data_folder
         self.split = split
         assert self.split in {'TRAIN', 'VAL', 'TEST'}
@@ -155,25 +163,22 @@ class NearestCaptionAvgDataset(Dataset):
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],  # mean=IMAGENET_IMAGES_MEAN, std=IMAGENET_IMAGES_STD
                                      std=[0.229, 0.224, 0.225])
         ])
+ 
 
     def __getitem__(self, i):
+        print("\n nearest caption dataset: caption i",i)
         # Remember, the Nth caption corresponds to the (N // captions_per_image)th image
         img = Image.open(self.data_folder+"/"+self.imgpaths[i // self.cpi])
+        print("path image i",self.imgpaths[i // self.cpi])
 
-
-        img = self.transform(img)
-
-        #nearest_image = retrival(image)
-        #nearest_caption 
-        #nearest caption -> média dos embeddings
-        #nearest = média dos embeddings
-        
+        img = self.transform(img)     
+            
         caption = torch.LongTensor(self.captions[i])
 
         caplen = torch.LongTensor([self.caplens[i]])
 
         if self.split == 'TRAIN':
-            return img, nearest, caption, caplen
+            return img, caption, caplen, i// self.cpi #n+ da imagem
         else:
             # For validation of testing, also return all 'captions_per_image' captions to find BLEU-4 score
             all_captions = torch.LongTensor(
@@ -186,7 +191,6 @@ class NearestCaptionAvgDataset(Dataset):
 
     def __len__(self):
         return self.dataset_size
-
 
 class CaptionDataset(Dataset):
     """
@@ -227,14 +231,9 @@ class CaptionDataset(Dataset):
         with open(os.path.join(data_folder, self.split + '_IMGPATHS_' + data_name + '.json'), 'r') as j:
             self.imgpaths = json.load(j)
 
-
         # Total number of datapoints
         self.dataset_size = len(self.captions)
 
-    def __getitem__(self, i):
-        # Remember, the Nth caption corresponds to the (N // captions_per_image)th image
-        img = Image.open(self.data_folder+"/"+self.imgpaths[i // self.cpi])
-  
         self.transform = transforms.Compose([
                 transforms.Resize(256),
                 transforms.CenterCrop(224),
@@ -242,6 +241,13 @@ class CaptionDataset(Dataset):
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],  # mean=IMAGENET_IMAGES_MEAN, std=IMAGENET_IMAGES_STD
                                      std=[0.229, 0.224, 0.225])
         ])
+
+    def __getitem__(self, i):
+        # Remember, the Nth caption corresponds to the (N // captions_per_image)th image
+        print("index of dataset", i)
+        
+        img = Image.open(self.data_folder+"/"+self.imgpaths[i // self.cpi])
+
 
         img = self.transform(img)
         
