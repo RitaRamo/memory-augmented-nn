22a927f4d0c4322f9b9c91b47ae36093b5a99d2a
RitaRamos <rita.mparada.ramos@gmail.com>
2020-12-27 23:41:41 +0000

    add proposed multi level attention mechanism

--- image_captioning/models.py
+++ image_captioning/models.py
@@ -88,6 +88,41 @@ class Attention(nn.Module):
         return attention_weighted_encoding, alpha
 
 
+# class RetrievedAttention(nn.Module):
+#     """
+#     Attention Network.
+#     """
+
+#     def __init__(self, encoder_dim, decoder_dim, attention_dim):
+#         """
+#         :param encoder_dim: feature size of encoded images
+#         :param decoder_dim: size of decoder's RNN
+#         :param attention_dim: size of the attention network
+#         """
+#         super(Attention, self).__init__()
+#         self.encoder_att = nn.Linear(encoder_dim, attention_dim)  # linear layer to transform encoded image
+#         self.decoder_att = nn.Linear(decoder_dim, attention_dim)  # linear layer to transform decoder's output
+#         self.full_att = nn.Linear(attention_dim, 1)  # linear layer to calculate values to be softmax-ed
+#         self.tanh = nn.Tanh()
+#         self.softmax = nn.Softmax(dim=1)  # softmax layer to calculate weights
+
+#     def forward(self, encoder_out, decoder_hidden):
+#         """
+#         Forward propagation.
+
+#         :param encoder_out: encoded images, a tensor of dimension (batch_size, num_pixels, encoder_dim)
+#         :param decoder_hidden: previous decoder output, a tensor of dimension (batch_size, decoder_dim)
+#         :return: attention weighted encoding, weights
+#         """
+#         att1 = self.encoder_att(encoder_out)  # (batch_size, num_pixels, attention_dim)
+#         att2 = self.decoder_att(decoder_hidden)  # (batch_size, attention_dim)
+#         att = self.full_att(self.tanh(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
+#         alpha = self.softmax(att)  # (batch_size, num_pixels)
+#         attention_weighted_encoding = (encoder_out * alpha.unsqueeze(2)).sum(dim=1)  # (batch_size, encoder_dim)
+
+#         return attention_weighted_encoding, alpha
+
+
 class DecoderWithAttention(nn.Module):
     """
     Decoder.
@@ -135,8 +170,8 @@ class DecoderWithAttention(nn.Module):
         self.fc = nn.Linear(decoder_dim, vocab_size)  # linear layer to find scores over vocabulary
         self.init_weights()  # initialize some layers with the uniform distribution
         
-        self.lookup_table= lookup_table #target lookup table of the nearest input examples
-        print("self.lookup_table just to check", self.lookup_table[:10])
+        self.target_lookup= lookup_table #target lookup table of the nearest input examples
+        print("self.lookup_table just to check", self.target_lookup[:10])
 
     def init_weights(self):
         """
@@ -184,7 +219,7 @@ class DecoderWithAttention(nn.Module):
         for p in self.embedding.parameters():
             p.requires_grad = fine_tune
 
-    def init_hidden_state(self, encoder_out, retrieved_index_of_nearest_imgs):
+    def init_hidden_state(self, encoder_out, retrieved_neighbors_index):
         """
         Creates the initial hidden and cell states for the decoder's LSTM based on the encoded images.
 
@@ -198,29 +233,24 @@ class DecoderWithAttention(nn.Module):
             c = self.init_c(mean_encoder_out)
 
         elif self.model_type == "SAR_avg":
-            targets_caps_of_nearest_imgs=self.lookup_table[retrieved_index_of_nearest_imgs*5]
-            print("target caps", targets_caps_of_nearest_imgs)
-            nearest_targets_embeddings = self.embedding(targets_caps_of_nearest_imgs).mean(1)
-            print("embed of near", nearest_targets_embeddings.size())
-            c = self.init_c(nearest_targets_embeddings)
-
-        elif self.model_type == "SAR_norm":
-            targets_caps_of_nearest_imgs=self.lookup_table[retrieved_index_of_nearest_imgs*5]
-            print("target caps", targets_caps_of_nearest_imgs)
-            nearest_targets_embeddings = self.embedding(targets_caps_of_nearest_imgs).mean(1)
-            print("embed of near", nearest_targets_embeddings.size())
-            c = self.init_c(nearest_targets_embeddings)
+            target_neighbors=self.target_lookup[retrieved_neighbors_index*5]
+            target_neighbors_representation = self.embedding(target_neighbors).mean(1)
+            
+            print("target caps", target_neighbors)
+            print("embed of near", target_neighbors_representation.size())
+            c = self.init_c(target_neighbors_representation)
 
         else:
             raise Exception ("no mode model")
         
         return h, c
 
-    def forward(self, encoder_out, retrieved_index_of_nearest_imgs, encoded_captions, caption_lengths):
+    def forward(self, encoder_out, retrieved_neighbors_index, encoded_captions, caption_lengths):
         """
         Forward propagation.
 
         :param encoder_out: encoded images, a tensor of dimension (batch_size, enc_image_size, enc_image_size, encoder_dim)
+        :param retrieved_neighbors_index: for each encoded image, the corresponding index of its nearest neighbour
         :param encoded_captions: encoded captions, a tensor of dimension (batch_size, max_caption_length)
         :param caption_lengths: caption lengths, a tensor of dimension (batch_size, 1)
         :return: scores for vocabulary, sorted encoded captions, decode lengths, weights, sort indices
@@ -238,13 +268,13 @@ class DecoderWithAttention(nn.Module):
         caption_lengths, sort_ind = caption_lengths.squeeze(1).sort(dim=0, descending=True)
         encoder_out = encoder_out[sort_ind]
         encoded_captions = encoded_captions[sort_ind]
-        retrieved_index_of_nearest_imgs = retrieved_index_of_nearest_imgs[sort_ind]
+        retrieved_neighbors_index = retrieved_neighbors_index[sort_ind]
 
         # Embedding
         embeddings = self.embedding(encoded_captions)  # (batch_size, max_caption_length, embed_dim)
 
         # Initialize LSTM state
-        h, c = self.init_hidden_state(encoder_out, retrieved_index_of_nearest_imgs)  # (batch_size, decoder_dim)
+        h, c = self.init_hidden_state(encoder_out, retrieved_neighbors_index)  # (batch_size, decoder_dim)
 
         # We won't decode at the <end> position, since we've finished generating as soon as we generate <end>
         # So, decoding lengths are actual lengths - 1
