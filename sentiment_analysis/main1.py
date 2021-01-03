9bd14f1a2b8033ab038f2e471d7caf6521ba11b5
RitaRamos <rita.mparada.ramos@gmail.com>
2021-01-03 19:37:24 +0000

    run new model

--- sentiment_analysis/main1.py
+++ sentiment_analysis/main1.py
@@ -45,7 +45,7 @@ DROPOUT = 0.5
 #device = "cpu"
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-MODEL_TYPE="SAR_avg"
+MODEL_TYPE="SAR_norm"
 MULTI_ATTENTION = True 
 DEBUG = False
 
