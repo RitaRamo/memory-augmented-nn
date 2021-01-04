d9443843077c003487d0c44b1ccc860cf67ee3bf
RitaRamos <rita.mparada.ramos@gmail.com>
2021-01-04 13:00:38 +0000

    run sar model

--- sentiment_analysis/main1.py
+++ sentiment_analysis/main1.py
@@ -46,7 +46,7 @@ DROPOUT = 0.5
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 MODEL_TYPE="SAR_norm"
-MULTI_ATTENTION = False
+MULTI_ATTENTION = True
 DEBUG = False
 
 #TODO: debug do retrieval
