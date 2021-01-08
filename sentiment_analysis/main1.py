5ff62ffd158dcbf0e941c2986ab5044bda230c17
RitaRamos <rita.mparada.ramos@gmail.com>
2021-01-08 11:08:48 +0000

    analyse params

--- sentiment_analysis/main1.py
+++ sentiment_analysis/main1.py
@@ -45,7 +45,7 @@ DROPOUT = 0.5
 #device = "cpu"
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-MODEL_TYPE="SAR_-11"
+MODEL_TYPE="SAR_bert"
 MULTI_ATTENTION = False
 DEBUG = False
 WITHOUT_RETRIEVED_MEMORY=False
