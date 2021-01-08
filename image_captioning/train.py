df1de007fbfc5be143d1d6b09334b85468c9b98e
RitaRamos <rita.mparada.ramos@gmail.com>
2021-01-08 12:53:37 +0000

    analyse params

--- image_captioning/train.py
+++ image_captioning/train.py
@@ -55,7 +55,7 @@ fine_tune_encoder = False  # fine-tune encoder?
 checkpoint = None  # path to checkpoint, None if none
 
 MULTILEVEL_ATTENTION = True
-MODEL_TYPE = "SAR_avg"
+MODEL_TYPE = "SAR_norm"
 #BASELINE
 #SAR_avg
 #SAR_norm
